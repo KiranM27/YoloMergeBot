41c4d449b7d4314c05e7fa7dbf9cb38db02e0c73
KiranMo27 <112458705+KiranMo27@users.noreply.github.com>
2024-07-05 12:59:39 +0800

    chore: use file helpers in the json file

--- modules/file_detector.py
+++ modules/file_detector.py
@@ -3,7 +3,9 @@
 # once the files are found, look at the file's content and see if it makes sense 
 # and also pull any other files that are related to the file that needs to be edited
 
+import json
 from modules.models import Models
+from modules.file_helpers import FileHelpers
 from modules.constants import FILE_DETECTION_SYSTEM_PROMPT, FILE_DETECTION_HUMAN_PROMPT
 from langchain_core.prompts.chat import ChatPromptTemplate
 
@@ -24,10 +26,16 @@ class FileDetector:
 
         self.chain = prompt | self.model
 
-        # fetch the metadata from the json 
+        # fetch the metadata from the json file
+        self.file_helpers = FileHelpers()
+        metadata_file_path = self.file_helpers.get_metadata_file_path()
+        metadata = self.file_helpers.get_file_content(metadata_file_path)
+
+        # parse the metadat into a json dict 
+        self.metadata = json.loads(metadata)
 
     def detect_files(self):
-        query = FILE_DETECTION_HUMAN_PROMPT.format(input=self.user_query)
+        query = FILE_DETECTION_HUMAN_PROMPT.format(input=self.user_query, metadata=self.metadata)
         response = self.chain.invoke({"input": query})
         content = response.content
         return content
