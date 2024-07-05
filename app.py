7031a189333d949f7fa8f1afbac02a343dadf9ba
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2024-07-05 08:49:37 +0800

    feat: extract content from files

--- app.py
+++ app.py
@@ -3,11 +3,23 @@ from modules.generate_repo_metadata import RepoMetaDataGenerator
 from pprint import pprint
 
 
-def runner():
-    repo_metadata_generator = RepoMetaDataGenerator(TRAGET_REPO_RELATIVE_PATH, TARGET_REPO_SRC_FOLDER)
-    files = repo_metadata_generator.list_all_files()
-    pprint(files)
+class Runner:
+    def generate_metadata(self):
+        repo_metadata_generator = RepoMetaDataGenerator(
+            TRAGET_REPO_RELATIVE_PATH, TARGET_REPO_SRC_FOLDER
+        )
+        files = repo_metadata_generator.list_all_files()
+
+        if not files:
+            print("No files found in the repo")
+            return
+
+        file = files[0]
+        file_path = file["file_path_from_here"]
+        file_content = repo_metadata_generator.get_file_content(file_path)
+        pprint(file_content)
 
 
 if __name__ == "__main__":
-    runner()
+    runner = Runner()
+    runner.generate_metadata()
--- modules/generate_repo_metadata.py
+++ modules/generate_repo_metadata.py
@@ -11,13 +11,40 @@ class RepoMetaDataGenerator:
         os.makedirs(DOCS_FOLDER, exist_ok=True)
 
     def list_all_files(self) -> list:
-        files = []  # list of tuples containing the relative path (from the root of the folder) and the name of the file
+        files = []  # list of dicts containing
+        # the path to the file from the src folder
+        # the path to the file from the root of the repo
+        # the path to the file from where this code is being executed
+        # the file name
 
-        # the crawl will only happen from the src folder though 
-        for root, _, filenames in os.walk(self.src_folder_path):
-            for filename in filenames:
-                relative_path = os.path.relpath(os.path.join(root, filename), self.repo_path)
-                files.append((relative_path, filename))
+        for root, _, file_names in os.walk(self.src_folder_path):
+            for file_name in file_names:
+                file_path = os.path.join(root, file_name)
+                file_path_from_src = os.path.relpath(file_path, self.src_folder_path)
+                file_path_from_root = os.path.relpath(file_path, self.repo_path)
+                file_path_from_here = os.path.relpath(file_path, os.getcwd())
+                files.append(
+                    {
+                        "file_name": file_name,
+                        "file_path_from_src": file_path_from_src,
+                        "file_path_from_root": file_path_from_root,
+                        "file_path_from_here": file_path_from_here,
+                    }
+                )
 
         return files
-       
+
+    def get_file_content(self, file_path: str) -> str:
+        with open(file_path, "r") as file:
+            return file.read()
+
+    def clean_up_file_content(self, file_content: str) -> str:
+        return file_content.replace("\n", " ").replace("\t", " ").replace("  ", " ")
+    
+    def get_metadata_from_llm(self, file_path: str, file_content: str) -> str:
+        pass
+    
+    def get_metadata(self, file_path: str) -> str:
+        file_content = self.get_file_content(file_path)
+        file_content = self.clean_up_file_content(file_content)
+        return file_content
