1b2e62e5c9b4a06edaab01ace4775a27787cebab
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2024-07-14 22:40:56 +0800

    feat: the ui kinda works

--- frontend/src/pages/app.tsx
+++ frontend/src/pages/app.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Inter } from 'next/font/google';
+import { ArrowRightCircleIcon } from '@heroicons/react/24/solid';
+import IconWrapper from '@/components/IconWrapper';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,23 +19,20 @@ const App: React.FC = () => {
 
   return (
     <div className={`h-screen w-screen bg-yolomergebot bg-cover flex justify-center items-center ${inter.className}`}>
-      {/* Centered container for the input */}
-      <div className="relative flex justify-center items-center">
-        {/* Image acting as the background for the input */}
-        <img
-          src="/images/app/text_box.jpeg" // Update with your image path
-          alt="center"
-          className="h-40 object-cover" // Adjust width and height as needed
-        />
-        {/* Transparent input box */}
-        <input
-          type="text"
-          value={input}
-          onChange={handleChange}
-          className="absolute w-80 bg-transparent text-black text-center border-none outline-none placeholder-black"
-          style={{ top: '50%', transform: 'translateY(-50%)' }} 
-        />
+      {/* Holding Container for the Input Box and the Send Button */}
+      <div className="flex flex-row gap-4 justify-center items-center">
+        <div className="relative">
+          {/* Image acting as the background for the input */}
+          <img src="/images/app/text_box.jpeg" alt="center" className="h-40 object-cover" />
+
+          <div className="h-full w-full absolute top-0 left-0 flex justify-center items-center px-8">
+            <input type="text" value={input} onChange={handleChange} className="w-full bg-transparent text-black text-center border-none outline-none placeholder-black " />
+          </div>
+          {/* Transparent input box */}
+        </div>
+        <ArrowRightCircleIcon className="h-24 w-24" />
       </div>
+
       {isLoading && <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-50 flex justify-center items-center">Loading...</div>}
     </div>
   );
